1fac3ec3689b97fb3dee57c25367bb8a4b1228d0
Dave! <60303855+DaveBit@users.noreply.github.com>
2022-06-22 09:17:25 +0200

    Add files via upload

---
+++ The Collatz Sequence.py
@@ -0,0 +1,27 @@
+def collatz(number):
+    if number % 2 == 0:
+        print (number // 2)
+        return number // 2
+    else:
+        print(3*number+1)
+        return 3*number+1
+
+x = -1
+while x == -1:
+    try:
+        print ('Insert a number')   
+        number = int(input())
+        x = 0
+    except ValueError:
+        print('should be an integer')
+
+
+while number != 1:
+           number = collatz(number)
+
+    
+    
+
+
+
+
